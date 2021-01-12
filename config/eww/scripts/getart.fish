2ba4a6880a16c29d8ecd47fa4af9e41bf0a99380
TessaiMusa <riyaz70307@gmail.com>
2021-01-12 15:54:49 +0530

    added some eww

---
+++ config/eww/scripts/getart.fish
@@ -0,0 +1,10 @@
+#!/bin/fish
+
+set tmp_dir '/tmp/eww/cover'
+if test -d $tmp_dir
+    mkdir -p $tmp_dir
+end
+
+if test (playerctl metadata --format {{mpris:artUrl}}) 
+    curl -s (playerctl metadata --format {{mpris:artUrl}}) --output $tmp_dir/cover.png
+end
